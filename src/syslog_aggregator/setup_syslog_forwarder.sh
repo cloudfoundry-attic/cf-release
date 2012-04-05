e121304acf6c960695d85c2e6eaea0bd120a3da7
Oleg Shaldybin <olegs@vmware.com>
2012-04-04 21:58:10 -0700

    Initial commit

---
+++ src/syslog_aggregator/setup_syslog_forwarder.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+if [ $# -ne 1 ]
+then
+    echo "Usage: setup_syslog_forwarder.sh [Config dir]"
+    exit 1
+fi
+
+CONFIG_DIR=$1
+
+# Place to spool logs if the upstream server is down
+mkdir -p /var/vcap/sys/rsyslog/buffered
+chown -R syslog:adm /var/vcap/sys/rsyslog/buffered
+
+cp $CONFIG_DIR/syslog_forwarder.conf /etc/rsyslog.d/00-syslog_forwarder.conf
+
+/usr/sbin/service rsyslog restart
