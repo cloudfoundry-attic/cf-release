9696a75b76e87954df4717a5d9f285c6ef635bac
Yang Yang <yangy@vmware.com>
2012-07-09 02:20:39 -0700

    prevent the racing condition where the chown of log directory was not finished
    Change-Id: I418ea84e8f145b6e5e7dcba4c5b9d1074b9a6821

--- jobs/vblob_node/templates/vblob_startup.sh
+++ jobs/vblob_node/templates/vblob_startup.sh
@@ -1,2 +1,12 @@
-touch /store/log/vblob.log
+# check whether the chown of /store has finished
+for n in `seq 1 100`; do
+  if [ -f /tmp/vcap_chown.out ]
+  then
+    touch /store/log/vblob.log
+    break
+  fi
+  sleep 0.1
+done
+
+# start the instance
 exec /usr/bin/node /var/vcap/packages/vblob/server.js -f /store/instance/config.json
