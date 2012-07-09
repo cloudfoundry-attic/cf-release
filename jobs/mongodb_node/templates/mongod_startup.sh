26605e194e00a9f1e0032ba518e47508201249db
Yang Yang <yangy@vmware.com>
2012-07-09 02:20:39 -0700

    prevent the racing condition where chown of log directory was not finished
    Change-Id: I418ea84e8f145b6e5e7dcba4c5b9d1074b9a6821

--- jobs/mongodb_node/templates/mongod_startup.sh
+++ jobs/mongodb_node/templates/mongod_startup.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-touch /store/log/mongodb.log
+for n in `seq 1 100`; do
+  if [ -f /tmp/vcap_chown.out ]
+  then
+    touch /store/log/mongodb.log
+    break
+  fi
+  sleep 0.1
+done
+
 mkdir -p /store/instance/data
 exec /usr/bin/mongod --config /etc/mongodb.conf
