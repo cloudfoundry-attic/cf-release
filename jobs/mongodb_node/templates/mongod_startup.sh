124a154b6d61c0943e56233d1c024122106f5ea7
Chunjie <czhu@vmware.com>
2012-08-21 02:50:14 -0700

    cf-release warden mongo 2.0 service
        - support mongo 2.0 service instance
        - enable journaling true/false config item
        - max_disk size enlarge if journaling true
    
    Change-Id: If65ef9c5f0abf0fa29007ac4090060e62e827cc7

--- jobs/mongodb_node/templates/mongod_startup.sh
+++ jobs/mongodb_node/templates/mongod_startup.sh
@@ -8,4 +8,18 @@ done
 
 touch /store/log/mongodb.log
 mkdir -p /store/instance/data
-exec /usr/bin/mongod --config /etc/mongodb.conf
+
+# mongod_startup.sh 18
+# mongod_startup.sh 20
+# mongod_startup.sh 20 --nojournal
+if [ $# -gt 0 ]; then
+  if [ $1 = "18" ]; then
+    exec /usr/share/mongodb/mongodb18/mongod --config /etc/mongodb.conf
+  elif [ $1 = "20" ]; then
+    if [ $# -gt 1 ] && [ $2 = "--nojournal" ]; then
+      exec /usr/share/mongodb/mongodb20/mongod --nojournal --config /etc/mongodb.conf
+    else
+      exec /usr/share/mongodb/mongodb20/mongod --config /etc/mongodb.conf
+    fi
+  fi
+fi
