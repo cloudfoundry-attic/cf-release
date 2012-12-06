56e3fdb2b3d46a763abb87e650bb3dc8d16803b6
Dr Nic Williams <drnicwilliams@gmail.com>
2012-12-05 17:46:59 -0800

    converge /var/vcap/data/sys -> /var/vcap/sys
    Change-Id: I222fd531929a3afa2c1f8a11d2846e3cd22c646b

--- jobs/router/templates/setup_trafficlog_forwarder.sh
+++ jobs/router/templates/setup_trafficlog_forwarder.sh
@@ -9,7 +9,7 @@ fi
 CONFIG_DIR=$1
 
 #To enable rsyslog to read "nginx/router.access.log" under this folder
-chmod o+x /var/vcap/data/sys/log/
+chmod o+x /var/vcap/sys/log/
 
 cp $CONFIG_DIR/trafficlog_forwarder.conf /etc/rsyslog.d/10-trafficlog_forwarder.conf
 
