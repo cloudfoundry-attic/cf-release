e64c0708f808fc42187e45af0056ffea5c05da0a
Gregg Van Hove and Tim Labeeuw <pair+gvanhove+tim@pivotallabs.com>
2013-08-27 14:32:03 -0700

    NATS stream forwarder is more resilient to slow consumer errors Do logging off of the main listener thread to lower the number of slow consumer errors Standardize stream forwarder log messages look more like other messages
    [finish #55851462]

--- jobs/syslog_aggregator/templates/nats_stream_forwarder.rb
+++ jobs/syslog_aggregator/templates/nats_stream_forwarder.rb
@@ -3,7 +3,7 @@
 require 'syslog'
 require 'nats/client'
 
-syslog = Syslog.open('vcap.nats', Syslog::LOG_PID, Syslog::LOG_USER)
+@syslog = Syslog.open('vcap.nats', Syslog::LOG_PID, Syslog::LOG_USER)
 
 %w[TERM INT].each do |sig|
   trap(sig) do
@@ -11,13 +11,31 @@ syslog = Syslog.open('vcap.nats', Syslog::LOG_PID, Syslog::LOG_USER)
   end
 end
 
-NATS.start(:uri => ARGV[0]) do
-  NATS.subscribe(">") do |message, _, subject|
-    begin
-      json = %Q[{"timestamp": #{Time.now.to_f},"subject": #{subject.inspect},"message": #{message.nil? ? 'null' : message.inspect.inspect}}]
-      syslog.log(Syslog::LOG_INFO, "%s", json)
-    rescue Exception => e
-      puts "Error logging to syslog #{e.inspect}\n  #{e.backtrace.join("\n  ")}"
+def log(level, timestamp, message, data)
+  syslog_level = level == :error ? Syslog::LOG_ERR : Syslog::LOG_INFO
+  @syslog.log(syslog_level, %Q[{"timestamp":%f,"source":"NatsStreamForwarder","log_level":%s,"message":%s,"data":%s}],
+            timestamp, level.to_s.inspect, message.inspect, data)
+end
+
+def json_safe(string)
+  string.nil? ? 'null' : string.inspect
+end
+
+begin
+  NATS.start(:uri => ARGV[0]) do
+    NATS.subscribe(">") do |message, reply_inbox, subject|
+      received_at = Time.now.to_f
+      EM.defer do
+        begin
+          json = %Q[{"nats_message": #{json_safe message},"reply_inbox":#{json_safe reply_inbox}}]
+          log(:info, received_at, subject, json)
+        rescue Exception => e
+          puts "Error logging to syslog #{e.inspect}\n  #{e.backtrace.join("\n  ")}"
+        end
+      end
     end
   end
+rescue NATS::ServerError => e
+  log(:error, Time.now.to_f, "nats.error", %Q[{"exception_message": #{json_save e.message}}])
+  retry
 end
