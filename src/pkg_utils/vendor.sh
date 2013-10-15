918b7a8ccf3633cda4c8f52abe7fa345e3f6decf
Alex Suraci and aram price <pair+alex+aprice@pivotallabs.com>
2013-10-15 11:23:06 -0700

    remove pkg_utils, use bundle package
    the nightmare is over
    
    [#56835342]

--- src/pkg_utils/vendor.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -x -u -e
-
-[ $# -eq 1 ] || ( echo "wrong number of arguments, expected 1 but got $#" ; exit 2 )
-what=$1
-ruby pkg_utils/transform_git_source $what $what/vendor/checkout
-rm -rf $what/vendor/checkout
-ruby pkg_utils/fetch_gems $what/Gemfile $what/Gemfile.lock $what/vendor/cache
