950cf00706007a477a66a74b75071fd7e9bc0ef7
Alex Suraci <asuraci@pivotallabs.com>
2013-12-18 16:44:00 -0800

    revert snapshotting for now
    There is a bug in Warden that causes it to leave unreferenced containers
    around. Having them cleared on Warden startup is a band-aid for now, so
    we can't do snapshot deploys.
    
    [#57361124 #62709652]
    
    Revert "fix dea drain script"
    
    This reverts commit c06d9e59c8fb1e751955057434015797f46d0611.
    
    Revert "update drain script to add snapshotting"
    
    This reverts commit b9aafcb775be7e140542ee587eb6579c26ff41f0.
    
    Revert "warden: don't destroy containers on startup"
    
    This reverts commit 9065ec60819476e4d30699273fe2cb12294f8d6b.

--- jobs/dea_next/templates/deterministic_drain.rb
+++ jobs/dea_next/templates/deterministic_drain.rb
@@ -2,133 +2,27 @@
 
 require "logger"
 require "fileutils"
-require "json"
 
 FileUtils.mkdir_p("/var/vcap/sys/log/dea_next")
+logger = Logger.new("/var/vcap/sys/log/dea_next/drain.log")
 
-DEA_PIDFILE = "/var/vcap/sys/run/dea_next/dea_next.pid"
-SHUTTING_DOWN_FILE = "/var/vcap/sys/run/dea_next/shutting_down"
-WARDEN_PIDFILE = "/var/vcap/sys/run/warden/warden.pid"
-SNAPSHOT_PATH = "/var/vcap/data/dea_next/db/instances.json"
+logger.info("Drain script invoked with #{ARGV.join(" ")}")
 
-class DEADrainer
-  def initialize(argv)
-    @argv = argv
-  end
+dea_pidfile = "/var/vcap/sys/run/dea_next/dea_next.pid"
 
-  def drain
-    logger.info("Drain script invoked with #{@argv.join(" ")}")
-
-    unless dea_pid
-      logger.info("DEA not running; not draining")
-      set_timeout(0)
-      return
-    end
-
-    unless warden_pid
-      logger.info("Warden not running; not draining")
-      set_timeout(0)
-      return
-    end
-
-    if shutting_down?
-      mark_as_shutting_down
-      evacuate
-    elsif staging_tasks.empty?
-      soft_shutdown
-    else
-      disable_dea
-    end
-  rescue => e
-    logger.info("Failed to drain: #{e}")
-    set_timeout(0)
-  end
-
-  private
-
-  def shutting_down?
-    job_change == "job_shutdown" || \
-      # see bosh bug #62604304
-      File.exists?(SHUTTING_DOWN_FILE)
-  end
-
-  def job_change
-    @argv.first
-  end
-
-  def mark_as_shutting_down
-    # see bosh bug #62604304
-    FileUtils.touch(SHUTTING_DOWN_FILE)
-  end
-
-  def evacuate
-    logger.info("Sending signal USR2 to DEA to trigger evacuation.")
-    Process.kill("USR2", dea_pid)
-
-    set_timeout(-5)
-  end
-
-  def soft_shutdown
-    logger.info("Sending signal KILL to DEA.")
-    Process.kill("KILL", dea_pid)
-
-    logger.info("Sending signal USR2 to Warden to trigger draining.")
-    Process.kill("USR2", warden_pid)
-
-    set_timeout(0)
-  end
-
-  def disable_dea
-    logger.info("Sending signal USR1 to DEA to take it out of placement pools.")
-    Process.kill("USR1", dea_pid)
-
-    set_timeout(-10)
-  end
-
-  def set_timeout(timeout)
-    if timeout >= 0
-      logger.info("Setting timeout to #{timeout}.")
-    else
-      logger.info("Telling agent to drain again in #{-timeout} seconds.")
-    end
-
-    puts timeout
-  end
-
-  def staging_tasks
-    snapshot ? snapshot["staging_tasks"] : []
-  end
-
-  def snapshot
-    @snapshot ||=
-      if File.exists?(SNAPSHOT_PATH)
-        JSON.load(File.open(SNAPSHOT_PATH))
-      end
-  end
-
-  def dea_pid
-    @dea_pid ||=
-      if File.exists?(DEA_PIDFILE)
-        File.read(DEA_PIDFILE).to_i
-      end
-  end
-
-  def warden_pid
-    @warden_pid ||=
-      if File.exists?(WARDEN_PIDFILE)
-        File.read(WARDEN_PIDFILE).to_i
-      end
-  end
-
-  def logger
-    @logger ||= Logger.new("/var/vcap/sys/log/dea_next/drain.log")
-  end
+if !File.exists?(dea_pidfile)
+  logger.info("DEA not running")
+  puts 0
+  exit 0
 end
 
-
 begin
-  DEADrainer.new(ARGV).drain
+  dea_pid = File.read(dea_pidfile).to_i
+  logger.info("Sending signal USR2 to DEA.")
+  Process.kill("USR2", dea_pid)
+  logger.info("Hey BOSH, call me back in 5s.")
+  puts -5
 rescue Errno::ESRCH => e
-  logger.info("Caught ESRCH: #{e}")
+  logger.info("Caught exception: #{e}")
   puts 0
 end
