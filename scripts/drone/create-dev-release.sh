84ed15dfc7a4f48251b7609a0a67b8b2443e76be
Dave Liebreich <dliebreich@pivotallabs.com>
2014-06-02 17:44:29 -0700

    Add ability to build dev release using drone.
    - Add Dockerfile used to generate image for drone.

---
+++ scripts/drone/create-dev-release.sh
@@ -0,0 +1,34 @@
+CF_RELEASE_OUT="../create-release.out"
+
+bosh -n create release --with-tarball | tee $CF_RELEASE_OUT
+
+EXIT_STATUS=${PIPESTATUS[0]}
+if [ ! "$EXIT_STATUS" = "0" ]; then
+  echo "Failed to Create CF Release"
+  exit $EXIT_STATUS
+fi
+
+VERSION=`grep "Release version" $CF_RELEASE_OUT | cut -d " " -f3`
+if [ "$VERSION" = "" ]; then
+  echo "No Release Version Found"
+  exit 1
+fi
+
+MANIFEST_YML=`grep "Release manifest" $CF_RELEASE_OUT  | cut -d " " -f3`
+if [ "$MANIFEST_YML" = "" ]; then
+  echo "No Release Manifest Found"
+  exit 1
+fi
+
+TARBALL=`grep "Release tarball" $CF_RELEASE_OUT | cut -d " " -f4`
+if [ "$TARBALL" = "" ]; then
+  echo "No Release Tarball Found"
+  exit 1
+fi
+
+mkdir -p output
+
+mv $MANIFEST_YML ./output/dev-manifest.yml
+mv $TARBALL ./output/dev-release.tgz
+
+exit 0
