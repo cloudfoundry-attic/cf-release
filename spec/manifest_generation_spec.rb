0cfbb4178b3c1398752138a5461c74de8078a586
James Myers and Matthew Sykes <matthew@pivotallabs.com>
2014-09-08 09:37:50 -0700

    Fix typo in manifest generation specs

--- spec/manifest_generation_spec.rb
+++ spec/manifest_generation_spec.rb
@@ -1,7 +1,7 @@
 require 'tempfile'
 
 describe "Manifest Generation" do
-  shared_examples "generating manifsts" do |infrastructure|
+  shared_examples "generating manifests" do |infrastructure|
     it "builds the correct manifest for #{infrastructure}" do
       example_manifest = Tempfile.new("example-manifest.yml")
       `./generate_deployment_manifest #{infrastructure} spec/fixtures/#{infrastructure}/cf-stub.yml > #{example_manifest.path}`
@@ -12,18 +12,18 @@ describe "Manifest Generation" do
   end
 
   context "aws" do
-    it_behaves_like "generating manifsts", "aws"
+    it_behaves_like "generating manifests", "aws"
   end
 
   context "warden" do
-    it_behaves_like "generating manifsts", "warden"
+    it_behaves_like "generating manifests", "warden"
   end
 
   context "openstack" do
-    it_behaves_like "generating manifsts", "openstack"
+    it_behaves_like "generating manifests", "openstack"
   end
 
   context "vsphere" do
-    it_behaves_like "generating manifsts", "vsphere"
+    it_behaves_like "generating manifests", "vsphere"
   end
 end
