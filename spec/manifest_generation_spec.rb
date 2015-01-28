cbd9aa4de3013ec6967da855c3b033ae7af26f02
Maya Rosecrance <mrosecrance@pivotal.io>
2015-01-28 14:43:11 -0700

    Bump src/loggregator
    Bump :
      James Myers:
         Move manifest-templates into loggregator
      Warren Fernandes:
         Fix flakey test suite.
    
    * Move lamb specific manifest template properties into loggregator submodule
    
    Signed-off-by: James Myers <jmyers@pivotal.io>

--- spec/manifest_generation_spec.rb
+++ spec/manifest_generation_spec.rb
@@ -1,6 +1,8 @@
 require 'support/yaml_eq'
+require 'support/lamb-properties'
 require 'yaml'
 require 'tempfile'
+require 'erb'
 
 describe "Manifest Generation" do
   shared_examples "generating manifests" do |infrastructure|
@@ -9,7 +11,9 @@ describe "Manifest Generation" do
       `./generate_deployment_manifest #{infrastructure} spec/fixtures/#{infrastructure}/cf-stub.yml > #{example_manifest.path}`
       expect($?.exitstatus).to eq(0)
 
-      expected = File.read("spec/fixtures/#{infrastructure}/cf-manifest.yml")
+      expected = File.read("spec/fixtures/#{infrastructure}/cf-manifest.yml.erb")
+      lamb_properties = LambProperties.new(infrastructure)
+      expected = ERB.new(expected).result(lamb_properties.get_binding)
       actual = File.read(example_manifest.path)
 
       expect(actual).to yaml_eq(expected)
