84dc692152bfaa7e1f14c16fa09912e080a05954
Steffen Uhlig and Zak Auerbach <pair+suhlig-ibm+zauerbach@pivotal.io>
2015-05-13 16:47:03 -0700

    Read SHA from config/blobs instead of the actual blob store
    * Travis does not sync blobs, so this test failed
    * New implementation reads the SHA from config/blobs.yml
    
    [#92742158]

--- spec/stacks_spec.rb
+++ spec/stacks_spec.rb
@@ -1,14 +1,16 @@
+require 'yaml'
+
 describe "Stack Receipts" do
 
   context "cflinux" do
     it "has the right sha" do
-     compare_sha('spec/fixtures/receipts/cflinuxfs2_receipt', 'blobs/rootfs/cflinuxfs2.tar.gz')
+     compare_sha('spec/fixtures/receipts/cflinuxfs2_receipt', 'rootfs/cflinuxfs2.tar.gz')
     end
   end
 
-  def compare_sha(expected_filename, actual_filename)
+  def compare_sha(expected_filename, blob_key)
      expected_sha = File.read(expected_filename).lines.first[/[a-z0-9]{40}/]
-     actual_sha = `shasum #{actual_filename}`[0..39]
+     actual_sha = YAML.load_file('config/blobs.yml')[blob_key]['sha']
      expect(actual_sha).to(eq(expected_sha))
   end
 end
