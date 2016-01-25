28d6893870678b030a5562afc691e5911881a772
Haydon Ryan <haydonryan@gmail.com>
2016-01-25 20:50:27 +0000

    Revert "Merge upstream changes"
    This reverts commit 94f597f5fc2b843a392c8b475e8946c35ec15622, reversing
    changes made to 2bc16a037641bea5e31e87caa341fea49eb89c07.

--- spec/manifest_generation_spec.rb
+++ spec/manifest_generation_spec.rb
@@ -1,4 +1,5 @@
 require 'support/yaml_eq'
+require 'support/lamb-properties'
 require 'yaml'
 require 'tempfile'
 require 'erb'
@@ -10,7 +11,9 @@ describe "Manifest Generation" do
       `./scripts/generate_deployment_manifest #{infrastructure} spec/fixtures/#{infrastructure}/cf-stub.yml > #{example_manifest.path}`
       expect($?.exitstatus).to eq(0)
 
-      expected = File.read("spec/fixtures/#{infrastructure}/cf-manifest.yml")
+      expected = File.read("spec/fixtures/#{infrastructure}/cf-manifest.yml.erb")
+      lamb_properties = LambProperties.new(infrastructure)
+      expected = ERB.new(expected).result(lamb_properties.get_binding)
       actual = File.read(example_manifest.path)
 
       expect(actual).to yaml_eq(expected)
---
+++ spec/support/lamb-properties.rb
@@ -0,0 +1,1 @@
+../../src/loggregator/manifest-templates/lamb-properties.rb
